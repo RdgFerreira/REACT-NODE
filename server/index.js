c074569211e9d03a972cd0517ba0cf25aa3e6f64
RdgFerreira <araujofro@gmail.com>
2021-06-30 09:10:17 -0300

    1 aula

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const app = require('./config/express-config');
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello, World!<h1>')
+    res.send('<h1>Hello, World<h1>')
 });
+
+app.get('/user', (req,res) => {
+    const pessoa = {
+        nome: 'Gabriel',
+        sobrenome: 'Sellin'
+    };
+
+    res.json(pessoa);
+})
 
 app.listen(3000, 'localhost', () => console.log('Servidor, rodando.'));
